f15adeb4b95c981a5e447b3de8e40b21c47b803b
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-02-15 12:07:01 -0500

    keys open doors

--- addresses.py
+++ addresses.py
@@ -1,4 +1,10 @@
 from geopy import geocoders
+import json
+
+api_key_from_file = ""
+with open('config.json') as json_file:  
+    data = json.load(json_file)
+    api_key_from_file = data["API_KEY"]
 
 i = open('importedAddresses.txt', 'r')
 #c = open('addressCoordinates.txt', 'w')
@@ -8,7 +14,7 @@ for line in i:
 	l1 = line.replace('"', '')
 	l2 = l1.replace('$', ', ')
 	list = l2.split(',')
-	g = geocoders.GoogleV3()
+	g = geocoders.GoogleV3(api_key=api_key_from_file)
 	place, (lat, lng) = g.geocode(list[0])
 	#c.write('%.5f %.5f' % (lat, lng)) #must make sure it is okay to remove commas in the string
 	#c.write('\n')
